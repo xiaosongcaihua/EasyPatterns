126056055b81331bdf06c854e5f6fa5abdbc7ce5
Komatsu <62858577@gitlab.com>
2025-05-22 17:42:25 +0800

    学习工厂方法模式

---
+++ Creational-pattern/工厂方法模式.go
@@ -0,0 +1,55 @@
+package main
+
+import "fmt"
+
+//<------抽象层------>
+
+type AbstractFactory interface {
+	CreateFruit()
+}
+type Fruit interface {
+	Show()
+}
+
+//<-------实现层----->
+type Apple struct {
+}
+
+func (a *Apple) Show() {
+	fmt.Println("this is apple show")
+}
+
+type Banana struct {
+}
+
+func (b *Banana) Show() {
+	fmt.Println("this is banana show")
+}
+
+type AppleFactory struct {
+}
+
+func (a *AppleFactory) CreateFruit() Fruit {
+	return new(Apple)
+}
+
+type BananaFactory struct {
+}
+
+func (b *BananaFactory) CreateFruit() Fruit {
+	return new(Banana)
+}
+
+/**
+工厂方法模式诠释了开闭原则和单一职责原则
+缺点：每次创建对象的时候都需要创建一个工厂
+*/
+func main() {
+	af := new(AppleFactory)
+	apple := af.CreateFruit()
+	apple.Show()
+
+	bf := new(BananaFactory)
+	banana := bf.CreateFruit()
+	banana.Show()
+}
